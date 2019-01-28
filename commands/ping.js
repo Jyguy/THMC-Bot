3523803c741ccd37acc72798397244b1ce6eced6
Jyguy <jskekkx@gmail.com>
2019-01-28 16:04:07 -0500

    Fix ping command, remove start script

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 module.exports = (Client, message, args) => {
-  return message.channel.send(`:stopwatch: Ping: \`${Math.round(Client.bot.ping * 100) / 100}ms\``);
+  return message.channel.send(`:stopwatch: Ping: \`${Math.round(Client.bot.ws.ping * 100) / 100}ms\``);
 };
 
 module.exports.help = {
