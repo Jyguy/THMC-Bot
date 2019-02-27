c3074ed4bb4afc18b095daa02388168eccaa45dc
Jyguy <jskekkx@gmail.com>
2019-02-27 01:42:37 -0500

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# New THMC Bot
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.config = require('./config.json');
+
+client.login(client.config.token);
