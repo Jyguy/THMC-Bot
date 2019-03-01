d78c2630a133e3b5f935bf276135b0833a99dafc
Jyguy <jskekkx@gmail.com>
2019-03-01 04:36:04 -0500

    Fixed typo in -help and also added missing reply in message channel when no arguments are input.

--- commands/help.js
+++ commands/help.js
@@ -13,12 +13,14 @@ exports.run = (client, message, args) => {
       .setDescription(commands.join('\n'))
       .setColor(0x00FF00);
 
-    return message.author.send(embed).catch(() => {
-      if (message.channel.permissionsFor(client.user).has('EMBED_LINKS')) return message.channel.send(embed.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL()));
+    return message.author.send(embed)
+      .then(() => message.channel.send(`${message.author.tag}, Check your DMs!`))
+      .catch(() => {
+        if (message.channel.permissionsFor(client.user).has('EMBED_LINKS')) return message.channel.send(embed.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL()));
 
-      const str = `**Commands List**\n\n${commnads.join('\n')}`;
-      return message.channel.send(str);
-    });
+        const str = `**Commands List**\n\n${commands.join('\n')}`;
+        return message.channel.send(str);
+      });
   } else {
     if (!message.channel.permissionsFor(client.user).has('EMBED_LINKS')) return message.reply(':x: I do not have enough permissions in this channel! Please make sure I have all the following permissions.\n\n`Embed Links`');
     const cmd = args[1].toLowerCase();
