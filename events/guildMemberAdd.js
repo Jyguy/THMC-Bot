91282586004995b1bd1345949abf741d41a90d6e
Jyguy <jskekkx@gmail.com>
2019-06-19 20:46:38 -0400

    Added bot prevention.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,9 +2,11 @@
 
 /**
  * @param {import('../structures/client.js')} client
+ * @param {import('discord.js').GuildMember} member
  */
 exports.run = (client, member) => {
   if (member.user instanceof client.Discord.ClientUser) return;
+  if (Date.now() - member.user.createdTimestamp < 1000 * 60 * 60 * 24) return member.kick('Account younger than 1 day');
 
   /** @type {import('discord.js').TextChannel} */
   const channel = member.guild.channels.find(c => c.type === 'text' && c.id === '516525421018480649');
